09c42f6f1d9c82e21a24fbe690dccfd2018640fc
avi-tonde <133945482+avi-tonde@users.noreply.github.com>
2023-05-31 10:38:49 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 const userChoice = document.querySelectorAll('[data-selection]');
 let rulesModal = document.getElementById("rulesModal");
 let modalBtn = document.getElementById("rulesModalBtn");
@@ -11,13 +9,11 @@ const userWinner = document.getElementById('userWinner');
 const computerWinner = document.getElementById('computerWinner');
 const res = document.getElementById('res');
 
-
-
+const computerScore = document.getElementById('computerScore');
+const yourScore = document.getElementById('yourScore');
 
 let computerScores = 0;
 let yourScores = 0;
-
-
 
 function computerChoice() {
     let number = Math.floor(Math.random() * 3);
@@ -36,10 +32,6 @@ function computerChoice() {
     return computerPick;
 }
 
-
-
-
-
 userChoice.forEach(userChoiceButton => {
     userChoiceButton.addEventListener('click', e => {
         const userChoice = userChoiceButton.dataset.selection
@@ -49,133 +41,76 @@ userChoice.forEach(userChoiceButton => {
     })
 })
 
-
-
-
-
 const winner = (userChoice, computerPick) => {
-
- 
-
- const computerScore = document.getElementById('computerScore');
- const yourScore = document.getElementById('yourScore');
-
-   if (userChoice === computerPick) {
-      
-    //    console.log('tie');
-       userbtn.style.display= "none";
-       displayWinner.style.display= "flex";
-       res.innerText="TIE";
-
-       document.getElementById("userChoiceImg").src = "images/"+userChoice+"1.svg";
-       document.getElementById("computerChoiceImg").src = "images/"+computerPick+"1.svg"; 
-   }
-   else if (userChoice === 'rock') {
-       if (computerPick === 'scissor') {
-        //    console.log('You Lost');
-           
-           userbtn.style.display= "none";
-           displayWinner.style.display= "flex";
-           computerWinner.classList.add('winner');
-           res.innerText="YOU LOST";
-           
-           document.getElementById("userChoiceImg").src = "images/rock1.svg";
-           document.getElementById("computerChoiceImg").src = "images/scissor1.svg";
-
-           computerScores++;
-           computerScore.textContent = computerScores;
-
-       } else {
-        //    console.log('You won');
-           userbtn.style.display= "none";
-           displayWinner.style.display= "flex";
-           userWinner.classList.add('winner');
-           res.innerText="YOU WIN";
-
-           document.getElementById("userChoiceImg").src = "images/scissor1.svg";
-           document.getElementById("computerChoiceImg").src = "images/rock1.svg";    
-           
-           yourScores++;
-           yourScore.textContent = yourScores;
-       }
-   } else if (userChoice === 'paper') {
-       if (computerPick === 'rock') {
-        //    console.log('You Won');
-           userbtn.style.display= "none";
-           displayWinner.style.display= "flex";
-           userWinner.classList.add('winner');
-           res.innerText="YOU WIN"; 
-
-           document.getElementById("userChoiceImg").src = "images/paper1.svg";
-           document.getElementById("computerChoiceImg").src = "images/rock1.svg";
-
-           yourScores++;
-           yourScore.textContent = yourScores;
-           
-       } else {
-        //    console.log('You Lost');
-           userbtn.style.display= "none";
-           displayWinner.style.display= "flex";
-           computerWinner.classList.add('winner');
-           res.innerText="YOU LOST";
-
-           document.getElementById("userChoiceImg").src = "images/rock1.svg";
-           document.getElementById("computerChoiceImg").src = "images/paper1.svg"; 
-           
-           computerScores++;
-           computerScore.textContent = computerScores;
-       
-       }
-   }
-   else if (userChoice === 'scissor') {
-       if (computerPick === 'paper') {
-        //    console.log('You won');
-           userbtn.style.display= "none";
-           displayWinner.style.display= "flex";
-           userWinner.classList.add('winner'); 
-           res.innerText="YOU WIN";
-
-           document.getElementById("userChoiceImg").src = "images/scissor1.svg";
-           document.getElementById("computerChoiceImg").src = "images/paper1.svg"; 
-
-           yourScores++;
-           yourScore.textContent = yourScores;
-          
-    
-       } else {
-        //    console.log('You lost');
-           userbtn.style.display= "none";
-           displayWinner.style.display= "flex";
-           computerWinner.classList.add('winner'); 
-           res.innerText="YOU LOST"; 
-
-           document.getElementById("userChoiceImg").src = "images/paper1.svg";
-           document.getElementById("computerChoiceImg").src = "images/scissor1.svg"; 
-
-           computerScores++;
-           computerScore.textContent = computerScores;
-        
-       }
-   }
+    if (userChoice === computerPick) {
+        userbtn.style.display = "none";
+        displayWinner.style.display = "flex";
+        res.innerText = "TIE";
+        document.getElementById("userChoiceImg").src = "images/" + userChoice + "1.svg";
+        document.getElementById("computerChoiceImg").src = "images/" + computerPick + "1.svg";
+    }
+    else if (userChoice === 'rock') {
+        if (computerPick === 'scissor') {
+            computerWin(userChoice);
+        } else {
+            userWin(userChoice);
+        }
+    } else if (userChoice === 'paper') {
+        if (computerPick === 'rock') {
+            userWin(userChoice);
+        } else {
+            computerWin(userChoice);
+        }
+    }
+    else if (userChoice === 'scissor') {
+        if (computerPick === 'paper') {
+            userWin(userChoice);
+        } else {
+            computerWin(userChoice);
+        }
+    }
 }
 
-function playAgain(){
-    userbtn.style.display= "flex";
-    displayWinner.style.display= "none";
+function playAgain() {
+    userbtn.style.display = "flex";
+    displayWinner.style.display = "none";
     computerWinner.classList.remove('winner');
     userWinner.classList.remove('winner');
+    document.getElementById("userChoiceImg").src = "";
+    document.getElementById("computerChoiceImg").src = "";
 }
 
-function openModal(){
-    rulesModal.style.display= "block";
+function openModal() {
+    rulesModal.style.display = "block";
 }
 
-closeModal.onclick = function closeModal(){
-    rulesModal.style.display= "none";
+closeModal.onclick = function closeModal() {
+    rulesModal.style.display = "none";
 }
 
-function updateWindow(){
-    userbtn.style.display= "none";
-    displayWinner.style.display= "flex";
+function userWin(userChoice) {
+    //    console.log('You won');
+    userbtn.style.display = "none";
+    displayWinner.style.display = "flex";
+    userWinner.classList.add('winner');
+    res.innerText = "YOU WIN";
+    document.getElementById("userChoiceImg").src = "images/" + userChoice + "1.svg";
+    document.getElementById("computerChoiceImg").src = "images/" + computerPick + "1.svg";
+    yourScores++;
+    yourScore.textContent = yourScores;
+}
+
+
+function computerWin(userChoice) {
+    userbtn.style.display = "none";
+    displayWinner.style.display = "flex";
     computerWinner.classList.add('winner');
+    res.innerText = "YOU LOST";
+    document.getElementById("userChoiceImg").src = "images/" + userChoice + "1.svg";
+    document.getElementById("computerChoiceImg").src = "images/" + computerPick + "1.svg";
+    computerScores++;
+    computerScore.textContent = computerScores;
 }
+
+
+
